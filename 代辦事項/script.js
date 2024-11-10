2f050bf0930e575d459794d47be96da66b74e8ae
ChiangL <lily28372@gmail.com>
2024-11-10 15:59:26 +0800

    20241110

--- 代辦事項/script.js
+++ 代辦事項/script.js
@@ -1,2 +1,51 @@
-const 任務 = document.querySelector("h3");
-console.log(任務.textContent);
+const 文字欄 = document.querySelector(".文字欄");
+const 清單 = document.querySelector(".清單");
+const 按鈕 = document.querySelector(".按鈕");
+
+
+function 清單任務() {
+    if (文字欄.value === "") {
+        return;
+    }
+
+    const 任務 = document.createElement("li");
+    任務.innerHTML = `
+        <input type="checkbox" class="打勾方塊">
+        <label>${文字欄.value}</label>
+        <button class="垃圾桶">🗑</button>`
+
+
+    const 垃圾桶 = 任務.querySelector(".垃圾桶");
+    const 打勾方塊 = 任務.querySelector(".打勾方塊");
+
+    垃圾桶.addEventListener("click", function () {
+        任務.remove();
+    });
+
+    打勾方塊.addEventListener("change", function () {
+        if (打勾方塊.checked) {
+            任務.style.textDecoration = "line-through";
+            任務.style.color = "#999";
+            清單.append(任務);
+        } else {
+            任務.style.textDecoration = "none";
+            任務.style.color = "";
+            清單.prepend(任務);
+        }
+    });
+
+
+
+
+    清單.append(任務);
+    文字欄.value = "";
+
+}
+
+文字欄.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
+        清單任務();
+    }
+});
+
+按鈕.addEventListener("click", 清單任務);
